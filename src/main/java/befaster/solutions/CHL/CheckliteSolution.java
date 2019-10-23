fc78a8b5a76d465df39292f76e90c6d869539ff8
sbx_user1051 <sbx_user1051@169.254.166.117>
2019-10-23 18:27:49 +0000

    Wed Oct 23 18:27:49 UTC 2019

--- src/main/java/befaster/solutions/CHL/CheckliteSolution.java
+++ src/main/java/befaster/solutions/CHL/CheckliteSolution.java
@@ -2,9 +2,39 @@ package befaster.solutions.CHL;
 
 import befaster.runner.SolutionNotImplementedException;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CheckliteSolution {
     public Integer checklite(String skus) {
+        Map<String, Integer> priceMap = new HashMap<>();
+        priceMap.put("A", 50);
+        priceMap.put("B", 30);
+        priceMap.put("C", 20);
+        priceMap.put("D", 15);
         String[] skusArr = skus.split(",");
+        Map<String, Integer> skuMap = new HashMap<>();
+        for(String sku : skusArr) {
+            if (skuMap.containsKey(sku)) {
+                Integer count = skuMap.get(sku);
+                skuMap.put(sku, count++);
+            } else {
+                skuMap.put(sku, 1);
+            }
+        }
+
+        Integer price = 0;
+        for (String sku : skuMap.keySet()) {
+            int qty = skuMap.get(sku);
+            if (sku.equalsIgnoreCase("A") && qty % 3 == 0) {
+                price = price + qty
+            }
+
+        }
+
+
+        return null;
 
     }
 }
+
