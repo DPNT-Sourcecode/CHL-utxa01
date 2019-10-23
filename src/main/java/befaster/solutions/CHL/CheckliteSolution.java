3f1284587c61ceaecf15b3261900603b9de2b84e
sbx_user1051 <sbx_user1051@169.254.166.117>
2019-10-23 19:24:46 +0000

    Wed Oct 23 19:24:46 UTC 2019

--- src/main/java/befaster/solutions/CHL/CheckliteSolution.java
+++ src/main/java/befaster/solutions/CHL/CheckliteSolution.java
@@ -6,10 +6,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class CheckliteSolution {
-    public static void main(String[] args) {
-        CheckliteSolution sol = new CheckliteSolution();
-        System.out.println(sol.checklite("FFABCDECBAABCABBAAAEEAAFF"));
-    }
     public Integer checklite(String skus) {
         Integer price = 0;
         if (skus.isEmpty()) {
@@ -70,6 +66,7 @@ public class CheckliteSolution {
         return price;
     }
 }
+
 
 
 
